8d8c464e45da1445d3071af5fefeb2a02e9cc100
techerss <www.donraph100@gmail.com>
2021-09-16 18:56:46 +0200

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "freelance";
+
+	$connect = mysqli_connect($servername, $username, $password, $dbname);
+
+
+?>
