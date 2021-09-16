b24f4585b988b582277beb47f82be11a0bb8c5da
techerss <www.donraph100@gmail.com>
2021-09-16 18:57:10 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,23 @@
+ <div class="foot">
+
+            <div class="container-fluid row d-flex justify-content-around">
+              <div class="col-md-4 col-sm-12 d-flex">
+                <h1 class="footlogo" >LTYAGE</h1>
+                <span class="footspan">&copy; LytAge Technologies Ltd 2021</span>
+              </div>
+
+              <div class="col-md-3 col-sm-12 d-flex ">
+                <div class="footicon">
+                  <a class="footlink" href=""><i class="fa fa-facebook"></i></a>
+                  <a class="footlink"  href=""><i class="fa fa-twitter"></i></a>
+                  <a class="footlink"  href=""><i class="fa fa-instagram"></i></a>
+                  <a class="footlink"  href=""><i class="fa fa-linkedin"></i></a>
+                </div>
+
+                <div class="footicon1">
+                  <a class="footlink footicon1"  href=""><i class="fa fa-dollar"> USD</i></a>
+                </div>
+              </div>
+
+
+            </div>
