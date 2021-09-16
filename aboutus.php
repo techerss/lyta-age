440d9d8bed8d7ba946038c786171f650d768c970
techerss <www.donraph100@gmail.com>
2021-09-16 18:56:08 +0200

    Create aboutus.php
    new add

---
+++ aboutus.php
@@ -0,0 +1,308 @@
+<?php
+  require_once('../includes/header1.php');
+
+?>
+
+
+          <!-- jumbotron section -->
+
+          <div class="jumbotron aboutjum" id="">
+
+            <div class="container " id="cap">
+              
+                <h1 class="jumtext">This Is Where You <br>Know <br> More About  LytAge</h1>
+
+                <form class="form-inline d-flex justify-content-center" action="">
+
+                    <div  id="btn">
+                      <a  class="btn btn-success" href="register.php">
+                        Get Started
+                    </a>
+                        <button class="btn btn-info">Watch Demo <i class="fa fa-play"></i> </button>
+
+                    </div>
+
+                </form>
+            </div>
+
+
+
+          </div>
+          <!-- modal -->
+          <!-- <button id="myBtn">Open Modal</button>
+          <div id="myModal" class="modal">
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <p>THIS IS MY MODAL BOX</p>
+            </div>
+
+          </div> -->
+
+          <div class="container bg-greys">
+
+            <div class="row">
+
+                <div class="col-md-6 welcome">
+
+                  <h1 class="wel text-center">Welcome to <span>L</span>yt<span>A</span>ge</h1>
+
+                  <p class="welp">LytAge is an ocean of infinite possibilities. It is a market place where skill meets money. 
+                    Talents are rewarded, and needs are certisfied. Take a deep dive into this ocean with one click!
+                  </p>
+
+                  <div class="d-flex justify-content-center">
+
+                    
+
+                    <a class="btn" id="join" href="register.php ?>">
+                      Join
+                  </a>
+                  </div>
+
+                </div>
+
+                <div class="col-md-6 img">
+
+                    <img src="../assets/img/images (5).jpeg" alt="" class="img-fluid" srcset="">
+
+                </div>
+
+            </div>
+
+          </div>
+
+          <div id="cont" class="container col-md-12 bg-light text-center">
+            <h1 class="text-center">-OUR MISSION-</h1><br>
+            <h5>Connecting Skill to Cash</h5><br>
+            <p id="pa" class="text-center container">Connectin skills to cash has never been more easier. with LytAge, every skill has a space
+              in the global market. Once you get a technical skill, or a soft skill, all you need to do is jump in, as we are the most 
+            willing to connect you to that source of your finance. We are here to serve you, we are here to build your brand and 
+          make it of global recorgnition, and relevance. </p>
+
+        </div>
+
+        <div class="container-fluid col-md-12 text-center">
+          <h1>-LEADERSHIP-</h1><br>
+          <div class="row">
+              
+              <div class="col-md-6">
+                  <img id="imgg" src="../assets/img/div1a.jpeg" alt="img" style="border-radius: 50%; width: 200px;"><br>
+                  <h5>DONRAPH</h5><br>
+
+                  <p id="pa">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor ipsum cum natus nulla, saepe, odit 
+                      quaerat modi neque tempora ducimus ullam fugit nesciunt rerum? Veritatis cupiditate nemo magnam amet delectus.</p>
+
+
+              </div>
+              <div class="col-md-6">
+                  <img src="../assets/img/div1b.jpeg" alt="c" style="border-radius: 50%; width: 200px; ">
+                  <h5>FORTSWIFT</h5><br>
+
+                  <p id="pa">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum magnam ut tempore sunt voluptatem.
+                      Enim cumque odio voluptate explicabo harum labore, nostrum ullam numquam exercitationem, autem voluptas vitae, cupiditate dolor!</p>
+
+              </div>
+            </div>
+
+            <div class="container-fluid col-md-12">
+                <h1>-TESTIMONIALS-</h1><br>
+                <div class="row ">
+                  <div class="col-md-4">
+                      <img src="../assets/img/div1c.jpeg" alt="" style="border-radius: 50%; width: 200px;">
+                      <h4>John</h4>
+                      <p id="pa">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus deserunt obcaecati dolor nisi animi dicta labore laboriosam 
+                          quam maiores repudiandae magni tempore voluptas amet, earum, repellendus distinctio unde nostrum sit?</p>
+
+
+                  </div>
+                  <div class="col-md-4">
+                      <img src="../assets/img/div1b.jpeg" alt="" style="border-radius: 50%; width: 200px; height: 160px;">
+                      <h4>Jessy</h4>
+                      <p id="pa">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, a soluta odit nisi eveniet nostrum numquam asperiores esse voluptates 
+                          itaque error commodi optio! Dolor vel corrupti itaque nam culpa praesentium.</p>
+
+                  </div>
+                  <div class="col-md-4">
+                      <img src="../assets/img/div1c.jpeg" alt=""  style="border-radius: 50%; width: 200px;" >
+                      <h4>Tom</h4>
+                      <p id="pa">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio consequatur nostrum similique! Corporis praesentium iste consequuntur
+                          nesciunt repellendus ducimus, quibusdam sunt. Voluptates laudantium natus, officiis illo libero ipsam magnam. Natus?</p>
+
+                  </div>
+                </div>
+            </div>
+        </div>
+
+         
+
+          <!-- sponsorship -->
+
+          <div class="container-fluid">
+            <div class="testbadge d-flex justify-content-center ">
+
+              <span class="badge bg">Sponsors</span>
+  
+              
+  
+            </div>
+
+            <div class="row d-flex justify-content-around text-center">
+
+              <div class="col-md-3 " ><img class="sponimg" src="../assets/img/pat1.jpeg" alt="" srcset=""></div>
+
+              <div class="col-md-3 "><img class="sponimg"  src="../assets/img/pat2.jpeg" alt="" srcset=""></div>
+
+              <div class="col-md-3"><img class="sponimg"  src="../assets/img/par3.jpeg" alt="" srcset=""></div>
+
+            </div>
+          </div>
+
+          <!-- scroll bar -->
+
+          <a class="scroll-to-top rounded" href="#page-top">
+            <i class="fas fa-angle-up"></i>
+          </a>
+
+          <hr>
+
+          <!-- footer -->
+
+          <footer class=" " >
+
+            <div class="row container-fluid d-flex justify-content-around footer">
+
+                <div class="col-md-2 footin">
+                  <h5  class="foothead">Categories</h5>
+                  <nav class="footnav">
+
+                    <ul>
+                      <li class="text-left"><a href="">Graphic Design</a> </li>
+                      <li class="text-left"><a href="">Digital Marketing</a> </li>
+                      <li class="text-left"><a href="">Wtriting & Translation</a></li>
+                      <li class="text-left"><a href="">Video & Animation</a></li>
+                      <li class="text-left"><a href="">Music & Audio</a></li>
+                      <li class="text-left"><a href="">Programming & Tech</a> </li>
+                      <li class="text-left"><a href="">Bussiness</a></li>
+                      <li class="text-left"><a href="">UI & UX</a></li>
+                    </ul>
+
+                  </nav>
+
+                </div>
+
+                <div class="col-md-2 footin">
+                  <h5 class="foothead">Support</h5>
+
+                  <nav class="footnav">
+
+                    <ul>
+                      <li class="text-left"><a href="">Help & Support</a> </li>
+                      <li class="text-left"><a href="">Trust & Safty</a> </li>
+                      <li class="text-left"><a href="">Selling on LytAge</a></li>
+                      <li class="text-left"><a href="">Buying on LytAge</a></li>
+                    
+                    </ul>
+
+                  </nav>
+
+                </div>
+
+                <div class="col-md-2 footin">
+                  <h5  class="foothead">Community</h5>
+
+                  <nav class="footnav">
+
+                    <ul>
+                      <li class="text-left"><a href="">Event</a> </li>
+                      <li class="text-left"><a href="">Blog</a> </li>
+                      <li class="text-left"><a href="">Forum</a></li>
+                      <li class="text-left"><a href="">Community Standards</a></li>
+                      <li class="text-left"><a href="">Podcast</a></li>
+                      <li class="text-left"><a href="">Affiliate</a> </li>
+                      <li class="text-left"><a href="">Invite a Friend</a></li>
+                      <li class="text-left"><a href="">Become a Seller</a></li>
+                      <li class="text-left"><a href="">LytAge Elevate</a></li>
+                    </ul>
+
+                  </nav>
+
+                </div>
+
+                <div class="col-md-2 footin">
+                  <h5  class="foothead">More From LtyAge</h5>
+
+                  <nav class="footnav">
+
+                    <ul>
+                      <li class="text-left"><a href="">LytAge Pro</a> </li>
+                      <li class="text-left"><a href="">LytAge Studios</a> </li>
+                      <li class="text-left"><a href="">LytAge Logo Maker</a></li>
+                      <li class="text-left"><a href="">LytAge Guide</a></li>
+                      <li class="text-left"><a href="">Get Inspired</a></li>
+                      <li class="text-left"><a href="">ClearVoice</a> </li>
+                      <li class="text-left"><a href="">AND CO</a></li>
+                      <li class="text-left"><a href="">Learn</a></li>
+                    </ul>
+
+                  </nav>
+
+                </div>
+            </div>
+
+          </footer>
+
+          <hr>
+
+
+          
+         <?php
+          require_once('../includes/footer.php');
+         ?>
+
+
+
+        </div>
+
+        <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Join us" below if you are ready to Join LytAge.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="dashboard/register.html">Join us</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+        <!-- <img class="socialImg" src="img/97-979298_social-media-icons-twitter-facebook-instagram-vertical-social.png " alt="" srcset=""> -->
+
+
+          
+         
+
+          
+
+          
+
+          
+
+        <!-- <script src="dashboard/vendor/jquery/jquery.min.js"></script>
+        <script src="dashboard/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="dashboard/vendor/jquery-easing/jquery.easing.min.js"></script>
+        <script src="dashboard/js/sb-admin-2.min.js"></script>   -->
+    
+<!-- mainjs -->
+    <script src="../assets/jquery ne/jquerynew.js"></script>
+    <script src="../assets/boot4/js/bootstrap.js"></script>
+    <script src="../assets/boot4/js/js/bootstrap.js"></script>
+    <script src="../assets/boot4/js/js/jquery-3.2.1.min.js"></script>
+</body>
+</html>
