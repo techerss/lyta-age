22c14844e397b7083a781605c6a78790710aa21d
techerss <www.donraph100@gmail.com>
2021-09-16 18:58:14 +0200

    Create loader.php

---
+++ loader.php
@@ -0,0 +1,35 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="../assets/boot4/css/bootstrap.css">
+    <link rel="stylesheet" href="../assets/boot4/css/font-awesome/css/font-awesome.css">
+</head>
+<body style="background: black;">
+    
+
+    <div class="continer img text-center" >
+       <i class="fa fa-spinner fa-spin fa-5x" style="color: rgb(247, 9, 9); margin-top: "></i>
+       <p style="color: white;">up in a munite</p>
+
+    </div>
+
+    
+    <script type="text/javascript" src="js/js/jquery-3.2.1.min.js"></script>
+  
+    <script type="text/javascript" src="js/js/bootstrap.min.js"></script>
+
+    <script>
+        setTimeout(function(){
+            // for javascript linking one page to another
+            window.location.href="index.php"
+
+        }, 1000)
+    </script>
+</body>
+</html>
